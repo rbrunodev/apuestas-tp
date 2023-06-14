fc4a3c6cb947527807098ccd8ecd718ada43cbf6
Tomás Conejeros <96243018+tommiwarren@users.noreply.github.com>
2023-06-14 19:06:50 -0300

    Se agrega la rama tomi con lor archivos apuestas.py y users_info.csv, siendo un un archivo con parte de mis funciones y el otro, un archivo con usuarios de prueba.

---
+++ apuestas.py
@@ -0,0 +1,53 @@
+from passlib.hash import ldap_pbkdf2_sha256
+
+def generar_encripcion(password) -> str:
+    cryppass = ldap_pbkdf2_sha256.hash(password)
+    print(cryppass)
+    return cryppass
+def buscar_credenciales(user,password) -> None:
+    #Acá va la linea para abrir el archivo, leerlo y verificar la existencia de ciertos datos.
+    user = input("Ingrese un nuevo de usuario: ")
+    #while user not in users.info.csv:
+        #id = input("Este usuario ya se encuentra asociado a una cuenta existente. Por favor, ingrese un nuevo usuario: ")
+    password = input("Ingrese una contraseña: ")
+    cryppass = generar_encripcion(password)
+    while password == "" or len(password) < 4: #or cryppass not in usersinfo:
+        password = input("La contraseña no es correcta, por favor, vuelva a ingresar la contraseña: ")
+    print(f"Bienvenido {user}!!!")
+def crear_credenciales() -> None:
+    #Acá va la linea para abrir el archivo, leerlo y verificar la existencia de ciertos datos.
+    id = input("Ingrese su correo electrónico: ")
+    #while id in users.info.csv:
+        #id = input("Este correo ya se encuentra asociado a una cuenta existente. Por favor, ingrese un nuevo correo: ")
+    user = input("Ingrese un nuevo de usuario: ")
+    #while user in users.info.csv:
+        #id = input("Este usuario ya se encuentra asociado a una cuenta existente. Por favor, ingrese un nuevo usuario: ")
+    password = input("Ingrese una contraseña: ")
+    while password == "" or len(password) < 4:
+        password = input("La contraseña debe ser de un mínimo de 4 caracteres, por favor, ingrese una nueva contraseña: ")
+    cryppass = generar_encripcion(password)
+    moneyspend = 0
+    lastgamble = 0
+    balance = 0
+    new_user = (f"{id},{user},{cryppass},{moneyspend},{lastgamble},{balance}")#En esta linea irian incluidas todas las variables que se van a escribir en el archivo.csv, ordenadas en un string compatible con el csv.
+    #En esta linea se abre el comando writeline para dejar registro de los datos de la cuenta creada.
+    #Finalmente en esta linea se cierra el archivo.
+def mostrar_graficos() -> None:
+    print()
+def main():
+    print("\nBienvenido a Jugarsela!!!")
+    choice = input("\nDigite:\n'i' si desea ingresar a una cuenta existente,\n'r' si desea registrar una cuenta nueva,\n'Enter' si desea salir. ")
+    while choice != "" and choice is str:
+        choice.lower()
+        if choice == "i":
+            print("ingreso a cuenta existente.\n")
+            user = input("Ingrese su usuario: ")
+            password = input("Ingrese su contraseña: ")
+            buscar_credenciales(user,password)
+        if choice == "r":
+            print("Registro de cuenta nueva.\n")
+            crear_credenciales()
+        else:
+            break
+
+main()
